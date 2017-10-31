565d3c4b7cc3e5824f5094da07685fe3d687df2b
Vaerel <vince67204@gmail.com>
2017-10-31 15:27:03 +0100

    Hello world, version 2 modifiée.

--- Helloworld.pas
+++ Helloworld.pas
@@ -2,8 +2,21 @@ program hello_world;
 
 uses crt;
 
+VAR
+	prixht : real;
+	prixttc : real;
+
+
+
 begin
 	clrscr;
+
 	writeln('Hello world !');
+	readln;
+
+	writeln ('Saisir un prix hors taxe');
+	readln(prixht);
+	prixttc := (prixht*1.2);
+	writeln ('Le prix TTC est :', prixttc:1:2);
 	readln;
 End.
